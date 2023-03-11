9edd668f4c4dee70bc84c541ab593e4fd72f0abe
Bader Mohammad <bader97alyami@hotmail.com>
2023-03-11 16:36:19 +0300

    first commit

---
+++ src/components/UserProjects.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Text,
+  SimpleGrid,
+  Box,
+  Spinner,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Grid,
+  GridItem,
+  chakra,
+  Image,
+  Flex,
+  Icon,
+  Button,
+  HStack,
+  InputLeftElement,
+  InputGroup,
+} from "@chakra-ui/react";
+import { BsFillBriefcaseFill } from "react-icons/bs";
+import { MdEmail, MdHeadset, MdLocationOn } from "react-icons/md";
+import { MDBIcon } from "mdb-react-ui-kit";
+import NavCompany from "./NavCompany";
+import FooterHome from "./FooterHome";
+import { SetStateAction, useEffect, useState } from "react";
+import { Search2Icon } from "@chakra-ui/icons";
+function UserProjects() {
+  return (
+    <div>
+
+
+
+
+        
+    </div>
+  )
+}
+
+export default UserProjects
