2980adc69fe7595f2a2546258b6ace325436b48b
Hatim <myformal.01@gmail.com>
2023-03-13 14:40:31 +0300

    hatim

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,28 @@ import UserProjects from './components/UserProjects';
  import ProfilePage from './components/ProfilePage';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
+import MyProjects from './components/MyProjects';
+import CompanyProjects from './components/CompanyProjects';
+import Addbootcamp from './components/Addbootcamp';
  
 
 function App() {
  
   return (
     <div >
+    
       <nav>
       <Nav/>
       </nav>
-<Routes>
+      {/* <Addbootcamp/> */}
+      {/* <CompanyProjects/> */}
+      <MyProjects/>
+{/* <Routes>
         <Route path='/userproject' element={<UserProjects/>}/> 
         <Route path='/Allproject' element={<AllProjectsPage/>}/> 
         <Route path='/ProfilePage' element={<ProfilePage/>}/> 
 
-        </Routes>
+        </Routes> */}
        <footer>
         <Footer/>
        </footer>
