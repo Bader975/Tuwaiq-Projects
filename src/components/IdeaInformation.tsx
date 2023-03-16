b98923b64605050b5f9a60a87b0643a5c9c40c59
Fahad Al Orman <fahadalomran19@gmail.com>
2023-03-16 14:07:25 +0300

    update stayle

--- src/components/IdeaInformation.tsx
+++ src/components/IdeaInformation.tsx
@@ -15,8 +15,8 @@ function IdeaInformation() {
          <Box minH='30vh' w={1000} m={'auto'} bg='white' borderRadius={10} pr={10}>
     <Box >
        
-       <Text textAlign={'center'} fontSize={'35px'} mb={10}>عنوان الفكرة</Text>
-       <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt fuga consequatur, officia architecto incidunt quis rerum eius omnis cum consequuntur blanditiis voluptatum alias ullam molestiae ratione exercitationem? Recusandae, provident molestiae?</Text>
+       <Text textAlign={"justify"} justifyContent={"center"} fontSize={'35px'} mb={10}>عنوان الفكرة</Text>
+       <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut rerum nulla quo maiores quod aut corrupti provident. Et aliquid, molestiae illo inventore possimus, dolor quam voluptatem debitis suscipit eligendi tenetur. Molestiae sapiente earum assumenda ad neque. Delectus deserunt rem esse eum voluptatibus optio repudiandae illo officia eaque, in est? Ipsam pariatur maxime nisi voluptatibus, dolorem soluta eum autem nobis et dolores illum totam obcaecati consequatur molestiae rerum voluptates placeat corporis nesciunt exercitationem? Quo veritatis facilis magnam explicabo qui nobis, dolores, deleniti rerum excepturi omnis delectus! Aliquam sequi tempore architecto aperiam necessitatibus, exercitationem minima ipsam cum? Quo ipsam quas atque temporibus.</Text>
       
    </Box>
      </Box>
